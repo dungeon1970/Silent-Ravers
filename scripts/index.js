684c472860464bb3a7a4a9f5804e88a1551e1b42
dungeon1970 <75203763+dungeon1970@users.noreply.github.com>
2021-05-16 16:17:46 +0100

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+window.onload=function(){
+
+    const showMenuToggle = document.getElementsByClassName('toggle-button')[0]
+    const navbarLinks = document.getElementById('navbar-links');
+    const backToTop = document.getElementById('back-top');
+    const menuItem = [].slice.call(document.getElementsByClassName("menu-item"));
+    let lastItem = menuItem[0];
+
+
+    showMenuToggle.addEventListener('click', () => {
+        toggleNav();
+    })
+
+
+    backToTop.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    })
+   
+    
+    menuItem.forEach((element, index) => {
+        element.addEventListener("click", () => {
+            lastItem.classList.remove('menu-item-click');
+            lastItem = element;
+            element.classList.add('menu-item-click');
+            toggleNav();
+        });
+    });
+ 
+   
+    function toggleNav() {
+        navbarLinks.classList.toggle('active')
+            lastItem.classList.remove('menu-item-click');
+        
+    }
+
+    window.onscroll = function() {
+        if (window.pageYOffset > 150) {
+           backToTop.style.display = "block";
+        }
+        else {
+         backToTop.style.display = "none";
+        }
+        lastItem.classList.remove('menu-item-click');
+     }
+
+
+}//end
+
+
+  
+
